64cb52fd63a9d932358b0e34fc55d970a83ee268
liang <2548225237@qq.com>
2022-05-30 12:04:02 +0800

    add psql hook

--- model/psql/init.go
+++ model/psql/init.go
@@ -1,6 +1,10 @@
 package psql
 
 import (
+	"context"
+	"log"
+
+	"github.com/gin-gonic/gin"
 	"github.com/go-pg/pg/v10"
 	"go-project/pkg/setting"
 )
@@ -16,4 +20,24 @@ func Setup() {
 		panic(err)
 	}
 	Pgdb = pg.Connect(opt)
+	Pgdb.AddQueryHook(&debugger{})
+}
+
+type debugger struct{}
+
+func (d *debugger) BeforeQuery(ctx context.Context, evt *pg.QueryEvent) (context.Context, error) {
+	q, err := evt.FormattedQuery()
+	if err != nil {
+		return ctx, err
+	}
+
+	if _, ok := ctx.(*gin.Context); ok {
+		log.Printf("pg query:%s", string(q))
+	}
+
+	return ctx, nil
+}
+
+func (d *debugger) AfterQuery(context.Context, *pg.QueryEvent) error {
+	return nil
 }
